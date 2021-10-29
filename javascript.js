abf569e0efb25d7db48172120520cb06f591cb23
Roque Sanchez <res.1984.res@gmail.com>
2021-10-28 22:41:57 -0300

    Mi primeros inicios con js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const nombre = prompt ("Ingrese su nombre");
+let anoNacimiento = prompt ("Hola "+ nombre + " vamos a calcular tu edad, para ello te pediremos que ingreses el año de nacimiento");
+let anoActual = 2021;
+
+edad = anoActual - anoNacimiento;
+
+alert(nombre + " tu edad es " + edad);
