b3f54f2fe74228a8a90f7b1301047eacffede507
Roque Sanchez <res.1984.res@gmail.com>
2021-11-16 00:38:22 -0300

    Trabajo Final primer parte

---
+++ galeria.js
@@ -0,0 +1,14 @@
+class Producto {
+    constructor (nombre, precio) {
+    this.nombre=nombre;
+    this.precio=precio;
+    }
+}
+
+
+do {
+    let article = prompt('Seleccione el articulo a agregar al carrito de compras')
+    let price = parseInt(promtp('Ingrese el valor del articulo'))
+    if(confirm('Desea agregar mas articulos a su carrito'))
+        addArticle
+} while (condition);
