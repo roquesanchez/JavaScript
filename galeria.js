ad7bad3d2566b9c4e538e10e33a650484771ddae
Roque Sanchez <res.1984.res@gmail.com>
2021-11-17 14:08:01 -0300

    correccion array

--- galeria.js
+++ galeria.js
@@ -1,67 +1,67 @@
 class Producto {
-    constructor (id, nombre, precio) {
-    this.id = parseInt(id);
-    this.nombre = nombre.toUpperCase();
-    this.precio = parseFloat(precio);
-    }
+  constructor(id, nombre, precio) {
+    this.id = id
+    this.nombre = nombre
+    this.precio = precio
+  }
 }
 
 const productos = []
- productos.push (new Producto (1, 'Platos', 1000));
- productos.push (new Producto (2, 'Escultura', 1500));
- productos.push (new Producto (3, 'Trofeos', 1200));
- productos.push (new Producto (4, 'Colgantes', 200));
- productos.push (new Producto (5, 'Cabezas', 1500));
- productos.push (new Producto (6, 'Cucardas', 900));
- productos.push (new Producto (7, 'Imanes', 200));
+productos.push(new Producto(1, 'Platos', 1000))
+productos.push(new Producto(2, 'Escultura', 1500))
+productos.push(new Producto(3, 'Trofeos', 1200))
+productos.push(new Producto(4, 'Colgantes', 200))
+productos.push(new Producto(5, 'Cabezas', 1500))
+productos.push(new Producto(6, 'Cucardas', 900))
+productos.push(new Producto(7, 'Imanes', 200))
 
- 
 class Carrito {
-    constructor() {
-        this.articulos = []
-        this.total = 0
-        this.cantidadArticulos = 0
+  constructor() {
+    this.articulos = []
+    this.total = 0
+    this.cantidadArticulos = 0
+  }
+  getArticulos() {
+    return this.articulos
+  }
+  setArticulos(producto) {
+    this.articulos.push(producto)
+  }
+  getTotal() {
+    return this.total
+  }
+  setTotal() {
+    let total = 0
+    if (this.articulos.length > 0) {
+      this.articulos.forEach((element) => (total += element.precio))
     }
-    getArticulos() {
-        return this.articulos
-    }
-    setArticulos(producto) {
-        this.articulos.push(producto)
-    }
-    getTotal() {
-        return this.total
-    }
-    setTotal() {
-        let total = 0
-        if (this.articulos.length > 0) {
-            this.articulos.forEach((element) => (total += element.precio))
-        }
-        this.total = total
-            
-let texto = " ";
-           
-Producto.forEach(element => {
-    texto = `\n ${element.nombre}. ${element.precio}.`
-});
+    this.total = total
+  }
+  setCantidadArticlos() {
+    this.cantidadArticulos = this.articulos.length
+  }
+}
 
-if (article >= productos.length)
-            
- 
- 
- 
- /* do {
-    let article = prompt('Seleccione el articulo a agregar al carrito de compras\n 1. Platos\n 2.Esculturas\n 3.Trofeos\n 4.Colgantes\n 5.Cabezas\n 6.Cucardas\n 7.Imanes')
-    switch (id) {
-        case 1: prompt(productos.producto)
-        case 2: 
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:
-    }
-    let price = parseInt(promtp('Ingrese el valor del articulo'))
-    if(confirm('Desea agregar mas articulos a su carrito')){
-        addArticle
-} while (condition);*/
- 
+////////////////////////////////////
+let texto = ''
+const carrito = new Carrito()
+let carritoLleno = false
+
+productos.forEach((element) => {
+  texto += `\n${element.id}. ${element.nombre}.`
+})
+
+do {
+  let article = prompt('Seleccione el articulo a agregar al carrito de compras' + texto)
+  if (article <= productos.length) {
+    carrito.articulos.push(productos[article - 1])
+    console.log(carrito)
+    if (confirm('Desea agregar más productos?')) {
+      carritoLleno = false
+    } else carritoLleno = true
+  } else alert('Ingrese un producto válido')
+} while (!carritoLleno)
+
+carrito.setTotal()
+carrito.setCantidadArticlos()
+console.log(carrito)
